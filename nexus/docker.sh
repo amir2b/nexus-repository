2daacaa6130f064f3334e1099fed785f4aba83d3
Amir Bashiri <bashiri.net@gmail.com>
2022-10-11 16:44:09 +0330

    fix auth

--- nexus/docker.sh
+++ nexus/docker.sh
@@ -6,14 +6,6 @@ cd "$(dirname "$0")/../" || exit
 NEXUS_API=http://127.0.0.1/service/rest/v1
 source .env
 
-if [ -z "$NEXUS_USERNAME" ]; then
-    read -p "Enter Nexus's username: " NEXUS_USERNAME
-fi
-
-if [ -z "$NEXUS_PASSWORD" ]; then
-    read -sp "Enter Nexus's password: " NEXUS_PASSWORD
-fi
-
 curl --silent -X PUT "$NEXUS_API/security/realms/active" -u $NEXUS_USERNAME:$NEXUS_PASSWORD -H 'Content-Type: application/json' -d '[
     "NexusAuthenticatingRealm", "NexusAuthorizingRealm", "DockerToken"
 ]'
--- nexus/initial.sh
+++ nexus/initial.sh
@@ -6,14 +6,6 @@ cd "$(dirname "$0")/../" || exit
 NEXUS_API=http://127.0.0.1/service/rest/v1
 source .env
 
-if [ -z "$NEXUS_USERNAME" ]; then
-    read -p "Enter Nexus's username: " NEXUS_USERNAME
-fi
-
-if [ -z "$NEXUS_PASSWORD" ]; then
-    read -sp "Enter Nexus's password: " NEXUS_PASSWORD
-fi
-
 echo
 echo "Delete old repositories"
 repos=( "maven-central" "maven-public" "maven-releases" "maven-snapshots" "nuget-group" "nuget-hosted" "nuget.org-proxy" )
